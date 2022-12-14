9acaa87f26afa543bb96771dacd3a689b6faa7e5
Artem <artem.semenov.101@gmail.com>
2022-12-14 17:40:40 +0200

    Finished slider and block with mooving images

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+/**
+ * SLIDER
+ */
+
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal", //vertical..
-  loop: true,
+  // loop: true,
   speed: 500,
   // If we need pagination
-  spaceBetween: 5,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -15,4 +18,48 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-scrollbar",
     draggable: true,
   },
+  slidesPerView: 1.23,
+  slideToClickedSlide: true,
+  centeredSlides: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.23,
+      spaceBetween: 5,
+    },
+    640: {
+      slidesPerView: 1.7,
+    },
+    1024: {
+      centeredSlides: false,
+      slidesPerView: 2.72,
+      spaceBetween: 20,
+    },
+  },
+});
+
+/**
+ * PHOTOS SECTION - MOVE ON SCROLL
+ */
+
+const photosBlock = document.getElementById("images-block-photos");
+const moovingBlocks = document.querySelectorAll(".mooving-block");
+const blockHeight = photosBlock.offsetHeight;
+let deviceDelta = 40;
+if (window.innerWidth > 1024) {
+  deviceDelta = 70;
+}
+const delimeter = blockHeight / deviceDelta;
+
+document.addEventListener("scroll", function (e) {
+  let blockPosition = photosBlock.getBoundingClientRect().top;
+  console.log(blockPosition);
+  num = blockPosition;
+  if (blockPosition > blockHeight) {
+    num = blockHeight;
+  } else if (blockPosition < 0) {
+    num = 1;
+  }
+  moovingBlocks.forEach(
+    (el) => (el.style.top = `${deviceDelta - num / delimeter}px`)
+  );
 });

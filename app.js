931b6d0087a8148d45e26b9c40a2e76639ea089f
Artem <artem.semenov.101@gmail.com>
2022-12-18 23:31:39 +0200

    Adjusted slider, added new testimonials, fixed little bugs

--- app.js
+++ app.js
@@ -10,21 +10,22 @@ const drug = document.getElementById("slider-drug");
 
 const moovingIndex = null;
 
-const sliderLine = document.getElementById('slider-line');
+const sliderLine = document.getElementById("slider-line");
 
-const paragraphs = document.querySelectorAll('.lighten-text')
+const paragraphs = document.querySelectorAll(".lighten-text");
 
-const sliderDrugLine = document.getElementById('slider-drug-line')
+const sliderDrugLine = document.getElementById("slider-drug-line");
 
+const sliderContentBlock = document.getElementById("third-page-bottom-content");
 
-const sliderContentBlock = document.getElementById('third-page-bottom-content');
-
-const firstBlock = document.getElementById('slider-block-1')
+const firstBlock = document.getElementById("slider-block-1");
 
 const activeTextDelimeter = window.innerWidth / 2;
 
+const header = document.getElementById("header");
 
-const header = document.getElementById('header')
+const windowHeight = window.innerHeight;
+const windowWidth = window.innerWidth;
 
 let numberDelta;
 let drugNumberDelimeter;
@@ -36,16 +37,15 @@ let index;
   numberDelimeter = 3.4; // default sliderLine move Index
   moveDrugDelimeter = 8.4 // Drug move index
 }
-else */ 
+else */
 
 let firstSwiper;
 
-
 firstSwiper = new Swiper(".third-page-slider-line", {
-    direction: "horizontal",
-    // loop: true,
-    speed: 500,
-    freeMode:  {
+  direction: "horizontal",
+  // loop: true,
+  speed: 500,
+  /*  freeMode:  {
       enabled: true,
       momentumRatio: 1,
       momentumBounce: true,
@@ -53,101 +53,205 @@ firstSwiper = new Swiper(".third-page-slider-line", {
       momentumVelocityRatio: 1,
       momentum: true,
 
-    },
+    }, */
 
-    mousewheel: {
-      releaseOnEdges: true,
-      sensitivity: 0.5,
-      eventsTarget: ".block-with-decor"
-    },
+  /*  mousewheel: {
+    releaseOnEdges: true,
+    sensitivity: 0.5,
+    eventsTarget: ".block-with-decor",
+  }, */
   /* mousewheel: {
     releaseOnEdges: true,
     eventsTarget: ".slider"
   }, */
   grabCursor: true,
-    scrollbar: {
-      el: ".third-page-slider-scrollbar",
-      draggable: true,
-   
+  scrollbar: {
+    el: ".third-page-slider-scrollbar",
+    draggable: true,
+  },
+
+  slidesPerView: 1.23,
+  slideToClickedSlide: true,
+  centeredSlides: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.8,
     },
-    
-    slidesPerView: 1.23,
-    slideToClickedSlide: true,
-    centeredSlides: true,
-    breakpoints: {
-      320: {
-        slidesPerView: 1.8,
-      },
-      640: {
-      },
-      1024: {
-       
-        centeredSlides: true,
-        slidesPerView: 2,
-       
-      },
+    640: {},
+    1024: {
+      spaceBetween: 80,
+      slidesPerView: 2,
     },
+    1300: {
+      spaceBetween: 100,
+      slidesPerView: 2,
+    },
+  },
+});
+
+// let y = document.querySelector('.center-trigger-2').getBoundingClientRect().top + window.scrollY
+
+if (windowWidth > 1024) {
+  //scrolling down
+  firstSwiper.on("activeIndexChange", function (e) {
+    // console.log(sliderWrapper.getBoundingClientRect().top, window.scrollY);
+    let index = firstSwiper.activeIndex;
+    let previousIndex = firstSwiper.previousIndex;
+    console.log(index);
+    console.log(previousIndex);
+
+    if (index === 0 && previousIndex === 3) {
+    } else if (
+      index ===
+      0 /*  && !isInView(document.querySelector('.center-trigger-1')) */
+    ) {
+      // document.getElementById("trigger-1").scrollIntoView();
+      document.querySelector(".center-trigger-1").getBoundingClientRect().top +
+        window.scrollY -
+        (windowHeight - 50);
+    } else if (
+      index === 1 &&
+      previousIndex <
+        index /* && !isInView(document.querySelector('.center-trigger-2')) */
+    ) {
+      window.scroll({
+        top:
+          document.querySelector(".center-trigger-2").getBoundingClientRect()
+            .top +
+          window.scrollY -
+          (windowHeight - 50),
+      });
+      //  document.getElementById("trigger-2").scrollIntoView();
+    } else if (
+      index === 1 &&
+      previousIndex >
+        index /* && !isInView(document.querySelector('.center-trigger-2')) */
+    ) {
+      window.scroll({
+        top:
+          document.querySelector(".center-trigger-2").getBoundingClientRect()
+            .top +
+          window.scrollY -
+          50,
+      });
+      //  document.getElementById("trigger-2").scrollIntoView();
+    } else if (
+      index === 2 &&
+      previousIndex <
+        index /* && !isInView(document.querySelector('.center-trigger-3')) */
+    ) {
+      window.scroll({
+        top:
+          document.querySelector(".center-trigger-3").getBoundingClientRect()
+            .top +
+          window.scrollY -
+          (windowHeight - 50),
+      });
+
+      //  document.getElementById("trigger-3").scrollIntoView();
+    } else if (
+      index === 2 &&
+      previousIndex >
+        index /* && !isInView(document.querySelector('.center-trigger-3')) */
+    ) {
+      window.scroll({
+        top:
+          document.querySelector(".center-trigger-3").getBoundingClientRect()
+            .top +
+          window.scrollY -
+          50,
+      });
+
+      //  document.getElementById("trigger-3").scrollIntoView();
+    } else if (
+      index ===
+      3 /*&&  !isInView(document.querySelector('.center-trigger-4')) */
+    ) {
+      console.log("scrolling to trigger 4");
+      // document.getElementById("trigger-4").scrollIntoView();
+      window.scroll({
+        top:
+          document.querySelector(".center-trigger-4").getBoundingClientRect()
+            .top +
+          window.scrollY -
+          (windowHeight - 50),
+      });
+    }
   });
 
-   firstSwiper.on('activeIndexChange', function(e) {
+  //scrolling up
+  /* firstSwiper.on("slidePrevTransitionStart", function (e) {
   // console.log(sliderWrapper.getBoundingClientRect().top, window.scrollY);
-    let index = firstSwiper.activeIndex;
-    console.log(index);
-    
-    if (index === 0 && !isInView(document.getElementById('trigger-1'))) {
-      document.getElementById('trigger-1').scrollIntoView()
-    } else if (index === 1 && !isInView(document.getElementById('trigger-2'))) {
-      document.getElementById('trigger-2').scrollIntoView()
-    } else if (index === 2 && !isInView(document.getElementById('trigger-3')) ) {
-      document.getElementById('trigger-3').scrollIntoView()
-    } else if( index === 3 && !isInView(document.getElementById('trigger-4'))) {
-      document.getElementById('trigger-4').scrollIntoView()
+  let index = firstSwiper.activeIndex;
+  console.log(index);
 
-    }
-
-    
+  if (
+    index === 0   && !isInView(document.querySelector('.center-trigger-1')) 
+  ) {window.scroll({
+    top:  document.querySelector('.center-trigger-1').getBoundingClientRect().top + window.scrollY - 50
   })
- 
+    
+    // document.getElementById("trigger-1").scrollIntoView();
+  } else if (
+    index === 1  && !isInView(document.querySelector('.center-trigger-2')) 
+  ) {
+    window.scroll({
+      top:  document.querySelector('.center-trigger-2').getBoundingClientRect().top + window.scrollY - 50
+    })
+    //  document.getElementById("trigger-2").scrollIntoView();
+  } else if (
+    index === 2 && !isInView(document.querySelector('.center-trigger-3')) 
+  ) {
+    window.scroll({
+      top: document.querySelector('.center-trigger-3').getBoundingClientRect().top + window.scrollY - 50
+    })
 
+    //  document.getElementById("trigger-3").scrollIntoView();
+  } else if (
+    index === 3 &&  !isInView(document.querySelector('.center-trigger-4')) 
+  ) {
+    window.scroll({
+      top:  document.querySelector('.center-trigger-4').getBoundingClientRect().top + window.scrollY - 50
+    })
+    // document.getElementById("trigger-4").scrollIntoView();
+  }
+});
+ */
+
+  // console.log(document.querySelector('.center-trigger-2').getBoundingClientRect().top)
   function isInView(el) {
     let bounding = el.getBoundingClientRect();
-    if (
-      bounding.top >= 0 &&
-      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-    ) {
-      return true
-    }
-    else return false;
+    if (bounding.top >= 0 && bounding.bottom <= window.innerHeight) {
+      return true;
+    } else return false;
   }
- 
 
-/* if (window.innerWidth > 1024) { */
+  /* if (window.innerWidth > 1024) { */
 
-  document.addEventListener('scroll', function(e) {
-   let blockPosition = sliderWrapper.getBoundingClientRect().top
+  document.addEventListener("scroll", function (e) {
+    let blockPosition = sliderWrapper.getBoundingClientRect().top;
     //determine slider position
-   if (blockPosition < 0 && blockPosition > -sliderWrapperHeight) {
- header.classList.add('hide') 
-} 
- else {
-  header.classList.remove('hide') 
- }
+    if (blockPosition < 100 && blockPosition > -sliderWrapperHeight) {
+      header.classList.add("hide");
+    } else {
+      header.classList.remove("hide");
+    }
 
-if(isInView(document.getElementById('trigger-1'))) {
-  firstSwiper.slideTo(0, 1000)
-  // console.log('We have reached first trigger');
-} else if(isInView(document.getElementById('trigger-2')))  {
-  firstSwiper.slideTo(1, 1000)
-  // console.log('We have reached second trigger');
-}else if(isInView(document.getElementById('trigger-3')))  {
-  firstSwiper.slideTo(2, 1000)
-  // console.log('We have reached third trigger');
-}else if(isInView(document.getElementById('trigger-4')))  {
-  firstSwiper.slideTo(3,1000)
-  // console.log('We have reached fourth trigger');
-}
+    if (isInView(document.querySelector(".center-trigger-1"))) {
+      firstSwiper.slideTo(0, 1000);
+      // console.log('We have reached first trigger');
+    } else if (isInView(document.querySelector(".center-trigger-2"))) {
+      firstSwiper.slideTo(1, 1000);
+      // console.log('We have reached second trigger');
+    } else if (isInView(document.querySelector(".center-trigger-3"))) {
+      firstSwiper.slideTo(2, 1000);
+      // console.log('We have reached third trigger');
+    } else if (isInView(document.querySelector(".center-trigger-4"))) {
+      firstSwiper.slideTo(3, 1000);
+      // console.log('We have reached fourth trigger');
+    }
 
-/*    let blockPosition = sliderWrapper.getBoundingClientRect().top
+    /*    let blockPosition = sliderWrapper.getBoundingClientRect().top
    //determine slider position
   if (blockPosition < 0 && blockPosition > -sliderWrapperHeight) {
 header.classList.add('hide')
@@ -160,7 +264,8 @@ firstSwiper.slideTo(3,1000);
     header.classList.remove('hide')
   }
   }) */
-  })
+  });
+}
 /* 
 else if (window.innerWidth < 1300) {
   index = 0.3
@@ -230,12 +335,13 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-// freeMode: true,
-/* mousewheel: {
+  // freeMode: true,
+  /* mousewheel: {
   releaseOnEdges: true,
   eventsTarget: ".slider"
 }, */
   scrollbar: {
+    enabled: true,
     el: ".swiper-scrollbar",
     draggable: true,
   },
@@ -249,6 +355,7 @@ const swiper = new Swiper(".swiper", {
     },
     640: {
       slidesPerView: 1.7,
+      spaceBetween: 5,
     },
     1024: {
       centeredSlides: false,
@@ -285,8 +392,7 @@ document.addEventListener("scroll", function (e) {
   );
 });
 
-
-/** 
+/**
  * SMOOTH SCROLL
  */
 class SmoothAnchorScroll {
@@ -300,7 +406,13 @@ class SmoothAnchorScroll {
         const target = document.querySelector(this.getAttribute("href"));
 
         if (target) {
-          target.scrollIntoView({
+          firstSwiper.disable();
+          setTimeout(() => {
+            firstSwiper.enable();
+            firstSwiper.slideTo(0, 1000);
+          }, 1000);
+          window.scroll({
+            top: 0,
             behavior: "smooth",
           });
         }
@@ -312,12 +424,9 @@ class SmoothAnchorScroll {
 const smoothAnchorScroll = new SmoothAnchorScroll();
 smoothAnchorScroll.init();
 
-/** 
+/**
  * MOBILE BURGER
  */
-const button = document.getElementById('burger-button')
+const button = document.getElementById("burger-button");
 
-button.addEventListener('click', function(e) {
-
-})
-
+button.addEventListener("click", function (e) {});

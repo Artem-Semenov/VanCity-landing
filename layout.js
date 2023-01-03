ec8a25738f715d2168f37a70325e463957068392
Artem <artem.semenov.101@gmail.com>
2023-01-03 17:40:19 +0200

    did first display of as-built.html, added fade in fade out functions for the full display slider

--- layout.js
+++ layout.js
@@ -153,3 +153,34 @@ function isInView(el) {
     return true;
   } else return false;
 }
+
+function fadeIn(el, display) {
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += 0.1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+function fadeOut(el) {
+  el.style.opacity = 1;
+
+  (function fade() {
+    if ((el.style.opacity -= 0.1) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+/* fadeOut(document.querySelector('.main-first-page'))
+
+setTimeout(() => {
+  fadeIn(document.querySelector('.main-first-page'), 'block')
+},1000)
+ */
